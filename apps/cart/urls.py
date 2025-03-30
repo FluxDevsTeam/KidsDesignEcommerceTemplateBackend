b42b0bb007a642b3844d596dd077792e553028bf
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-30 23:16:09 +0100

    Add initial serializers and URL routing for cart app

---
+++ apps/cart/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+
+urlpatterns = [
+    # path('cart/', include(('apps.cart.urls', 'flightapp'), namespace='flightapp')),
+    # path('orders/', include(('apps.orders.urls', 'route'), namespace='route')),
+]
