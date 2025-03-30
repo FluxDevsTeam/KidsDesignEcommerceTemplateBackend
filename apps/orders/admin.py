bd27b282179106f0f83421b637eefc5352929f12
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-30 23:16:20 +0100

    Register product-related models in the admin interface

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from apps.products.models import ProductCategory, ProductSubCategory, ProductSize, Product
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductSubCategory)
+admin.site.register(ProductSize)
+admin.site.register(ProductCategory)
