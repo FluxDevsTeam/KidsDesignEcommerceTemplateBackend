c6e922d56f76831bf74d67424f8154879f5a90a8
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-31 10:46:19 +0100

    Update URL routing for product app; adjust namespaces for clarity

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('cart/', include(('apps.cart.urls', 'flightapp'), namespace='flightapp')),
-    path('orders/', include(('apps.orders.urls', 'route'), namespace='route')),
-    path('payment/', include(('apps.payment.urls', 'route'), namespace='route')),
-    path('products/', include(('apps.products.urls', 'route'), namespace='route')),
+    # path('cart/', include(('apps.cart.urls', 'cart'), namespace='cart')),
+    # path('orders/', include(('apps.orders.urls', 'orders'), namespace='orders')),
+    # path('payment/', include(('apps.payment.urls', 'payment'), namespace='payment')),
+    path('product/', include(('apps.products.urls', 'products'), namespace='products')),
 ]
