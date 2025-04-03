a991324bf7e6fb8c314761ac0f7ef5b45bb8f6a3
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-04-03 23:31:57 +0100

    Add nested routing for Cart and CartItem APIs and implement Swagger helper function

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
+from .views import ApiCart, ApiCartItem
+
+router = DefaultRouter()
+router.register("", ApiCart, basename="cart")
+cart_router = NestedDefaultRouter(router, "", lookup='cart')
+cart_router.register("items", ApiCartItem, basename="cart_item")
 
 urlpatterns = [
-    # path('cart/', include(('apps.cart.urls', 'flightapp'), namespace='flightapp')),
-    # path('orders/', include(('apps.orders.urls', 'route'), namespace='route')),
+    path("", include(router.urls)),
+    path("", include(cart_router.urls)),
 ]
