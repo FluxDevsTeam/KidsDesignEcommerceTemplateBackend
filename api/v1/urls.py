31c5bfad42c44188d76a2e152d0d4bd3f3a867c3
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-04-03 23:31:07 +0100

    Update URL patterns to remove commented-out code and include wishlist endpoint

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    # path('cart/', include(('apps.cart.urls', 'cart'), namespace='cart')),
-    # path('orders/', include(('apps.orders.urls', 'orders'), namespace='orders')),
+    path('cart/', include('apps.cart.urls')),
+    path('orders/', include(('apps.orders.urls', 'orders'), namespace='orders')),
     # path('payment/', include(('apps.payment.urls', 'payment'), namespace='payment')),
-    path('product/', include(('apps.products.urls', 'products'), namespace='products')),
+    path('product/', include('apps.products.urls')),
+    path('wishlist/', include('apps.wishlist.urls')),
 ]
