595e51f2212e6066b3fd19955cff7105b772a9de
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-04-03 23:33:44 +0100

    Add payment app permissions and refactor swagger_helper for improved readability

--- apps/products/utils.py
+++ apps/products/utils.py
@@ -2,39 +2,18 @@ from drf_yasg.utils import swagger_auto_schema
 from .pagination import PAGINATION_PARAMS
 
 
-def swagger_helper(tags, model, type):
-    configs = {
-        "list": {
-            "manual_parameters": PAGINATION_PARAMS,
-            "operation_id": f"list {model}",
-            "operation_description": f"Retrieve a list of {model}",
-        },
-        "retrieve": {
-            "operation_id": f"retrieve {model}",
-            "operation_description": f"Retrieves details of a specific {model}",
-        },
-        "create": {
-            "operation_id": f"create {model}",
-            "operation_description": f"Create a new {model}",
-        },
-        "partial_update": {
-            "operation_id": f"update {model}",
-            "operation_description": f"Update a {model}",
-        },
-        "destroy": {
-            "operation_id": f"delete {model}",
-            "operation_description": f"Delete a {model}",
-        },
-        "default": {
-            "operation_id": f"{model}",
-            "operation_description": f"{model}",
+def swagger_helper(tags, model):
+    def decorators(func):
+        descriptions = {
+            "list": f"Retrieve a list of {model}",
+            "retrieve": f"Retrieve details of a specific {model}",
+            "create": f"Create a new {model}",
+            "partial_update": f"Update a {model}",
+            "destroy": f"Delete a {model}",
         }
-    }
 
-    config = configs.get(type, configs["default"])
-    config["tags"] = [tags]
+        action_type = func.__name__
+        get_description = descriptions.get(action_type, f"{action_type} {model}")
+        return swagger_auto_schema(manual_parameters=PAGINATION_PARAMS, operation_id=f"{action_type} {model}", operation_description=get_description, tags=[tags])(func)
 
-    def decorator(view_func):
-        return swagger_auto_schema(**config)(view_func)
-
-    return decorator
+    return decorators
