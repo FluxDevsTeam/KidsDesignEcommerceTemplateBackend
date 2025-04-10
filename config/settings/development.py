7d83384aaec73c1a95d864e28ab33fd262094729
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-04-10 22:44:23 +0100

    Add caching configuration and ensure consistent Celery result backend settings in development and production

--- config/settings/development.py
+++ config/settings/development.py
@@ -26,3 +26,13 @@ DEFAULT_FROM_EMAIL = os.getenv("EMAIL")
 # celery for development (Windows pc)
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
--- config/settings/production.py
+++ config/settings/production.py
@@ -29,3 +29,13 @@ DEFAULT_FROM_EMAIL = os.getenv("EMAIL")
 # celery for prod (docker)
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
 CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
