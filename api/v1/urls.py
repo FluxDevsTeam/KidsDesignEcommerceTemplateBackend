30ae1414c66e635ac30a8cb7dc926e110df9b634
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-04-14 01:59:28 +0100

    Uncomment payment URL inclusion in urlpatterns

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('cart/', include('apps.cart.urls')),
     path('orders/', include(('apps.orders.urls', 'orders'), namespace='orders')),
-    # path('payment/', include(('apps.payment.urls', 'payment'), namespace='payment')),
+    path('payment/', include(('apps.payment.urls', 'payment'), namespace='payment')),
     path('product/', include('apps.products.urls')),
     path('wishlist/', include('apps.wishlist.urls')),
 ]
