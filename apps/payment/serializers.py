9ead0f543158f2d5bcf750ccbb77a7940562aa21
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-04-21 22:22:46 +0100

    Refactor payment serializers to enhance clarity and maintainability

--- apps/payment/serializers.py
+++ apps/payment/serializers.py
@@ -11,14 +11,14 @@ class PaymentCartItemSerializer(serializers.ModelSerializer):
 
 
 class PaymentCartSerializer(serializers.ModelSerializer):
-    cart_items = PaymentCartItemSerializer(many=True, read_only=True, source="cartitem_cart")
+    # cart_items = PaymentCartItemSerializer(many=True, read_only=True, source="cartitem_cart")
     subtotal = serializers.SerializerMethodField()
     total = serializers.SerializerMethodField()
     provider = serializers.ChoiceField(choices=[("flutterwave", "Flutterwave"), ("paystack", "Paystack")], default="flutterwave", write_only=True)
 
     class Meta:
         model = Cart
-        fields = ["delivery_fee", "cart_items", "subtotal", "total", "provider"]
+        fields = ["delivery_fee", "subtotal", "total", "provider"]
 
     def get_subtotal(self, obj):
         return sum(item.product.price * item.quantity for item in obj.cartitem_cart.all())
