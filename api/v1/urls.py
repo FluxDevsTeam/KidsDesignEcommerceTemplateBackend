9b153345eb7c36267c10ff1b0d592e630f0dc3d2
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-05-02 19:56:57 +0100

    Add admin module with initial configuration and URL routing

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('admins/', include('apps.admin.urls')),
     path('cart/', include('apps.cart.urls')),
     path('orders/', include(('apps.orders.urls', 'orders'), namespace='orders')),
     path('payment/', include(('apps.payment.urls', 'payment'), namespace='payment')),
