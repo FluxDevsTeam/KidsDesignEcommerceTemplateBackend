d56183aed04feb837876adeb056af45b1d96882c
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-05-02 19:57:27 +0100

    Add initial migration and views for admin order management

---
+++ apps/admin/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ApiAdminOrder, OrderDashboard
+
+router = DefaultRouter()
+router.register("order", ApiAdminOrder, basename="admin_order_page")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path('admin-dashboard/', OrderDashboard.as_view({'get': 'retrieve'}), name='admin_dashboard_page'),
+]
