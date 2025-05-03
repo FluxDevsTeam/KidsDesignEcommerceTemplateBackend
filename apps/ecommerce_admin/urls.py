a01ca62b50efe9e61c64aadf949d9b66da1ef3ab
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-05-03 22:39:00 +0100

    feat: Add API endpoints for Admin, Delivery, and Developer settings in urls.py

--- apps/ecommerce_admin/urls.py
+++ apps/ecommerce_admin/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ApiAdminOrder, OrderDashboard
+from .views import ApiAdminOrder, OrderDashboard, ApiAdminSettings, ApiDeliverySettings, ApiDeveloperSettings
 
 router = DefaultRouter()
 router.register("order", ApiAdminOrder, basename="admin_order_page")
@@ -8,4 +8,7 @@ router.register("order", ApiAdminOrder, basename="admin_order_page")
 urlpatterns = [
     path("", include(router.urls)),
     path('ecommerce_admin-dashboard/', OrderDashboard.as_view({'get': 'retrieve'}), name='admin_dashboard_page'),
+    path('admin-settings/', ApiAdminSettings.as_view({'get': 'list', 'patch': 'partial_update'}), name='admin_settings'),
+    path('delivery-settings/', ApiDeliverySettings.as_view({'get': 'list', 'patch': 'partial_update'}), name='delivery_settings'),
+    path('developer-settings/', ApiDeveloperSettings.as_view({'get': 'list', 'patch': 'partial_update'}), name='developer_settings'),
 ]
