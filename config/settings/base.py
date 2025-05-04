b5a8514246938b8c871c96ce46f69f50aeb687da
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-05-04 19:54:16 +0100

    feat: Add CACHE_TIMEOUT setting to manage cache duration from environment variables

--- config/settings/base.py
+++ config/settings/base.py
@@ -175,6 +175,7 @@ PAYMENT_IMAGE_URL = os.getenv("PAYMENT_IMAGE_URL")
 SITE_URL = os.getenv("SITE_URL")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
+CACHE_TIMEOUT = os.getenv("CACHE_TIMEOUT")
 
 # Time & retry settings
 CELERY_TIMEZONE = 'Africa/Lagos'
