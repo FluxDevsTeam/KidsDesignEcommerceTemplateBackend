b2e9b69ad75394b5306ba21614947be814c6b773
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-05-07 18:34:17 +0100

    changed product size

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -7,7 +7,6 @@ router = DefaultRouter()
 router.register("sub-category", ApiProductSubCategory, basename="product_sub_category")
 router.register("category", ApiProductCategory, basename="product_category")
 router.register("item", ApiProduct, basename="product_items")
-router.register("size", ApiProductSize, basename="product_size")
 
 product_router = NestedDefaultRouter(router, 'item', lookup='item')
 product_router.register('size', ApiProductSize, basename='product_size')
