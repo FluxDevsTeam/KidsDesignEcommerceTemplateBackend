1216b8bd647b0f806b02e1b5b7ee0e250880e4f2
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-05-22 19:07:42 +0100

    added admin search

--- apps/products/views.py
+++ apps/products/views.py
@@ -394,6 +394,40 @@ class ApiProduct(viewsets.ModelViewSet):
         cache.set(cache_key, response_data, cache_timeout)
         return Response(response_data, status=status.HTTP_200_OK)
 
+    # admin search also searches for unavailable products. that's the only difference
+    @swagger_auto_schema(manual_parameters=[openapi.Parameter('search', openapi.IN_QUERY, description="Search keyword", type=openapi.TYPE_STRING), openapi.Parameter('page', openapi.IN_QUERY, description="Page number", type=openapi.TYPE_INTEGER), openapi.Parameter('page_size', openapi.IN_QUERY, description="Items per page (max: 100)", type=openapi.TYPE_INTEGER)], operation_id="Admin Search Products", operation_description="Search and paginate products", tags=["Product"])
+    @action(detail=False, methods=['get'], url_path='admin-search')
+    def admin_search(self, request, *args, **kwargs):
+        query = request.query_params.get("search", "").strip()
+        cache_timeout = TIMEOUT
+        cache_params = dict(request.query_params)
+        cache_key = f"product_search:{json.dumps(cache_params, sort_keys=True)}"
+        cached_response = cache.get(cache_key)
+        if cached_response:
+            return Response(cached_response)
+
+        if not query:
+            return Response({"count": 0, "next": None, "previous": None, "results": []})
+
+        search_data = self.get_queryset().filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query) |
+            Q(sizes__size__icontains=query) |
+            Q(sub_category__name__icontains=query) |
+            Q(sub_category__category__name__icontains=query),
+        ).distinct()
+
+        page = self.paginate_queryset(search_data)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            response_data = self.get_paginated_response(serializer.data).data
+        else:
+            serializer = self.get_serializer(search_data, many=True)
+            response_data = serializer.data
+
+        cache.set(cache_key, response_data, cache_timeout)
+        return Response(response_data, status=status.HTTP_200_OK)
+
     @swagger_auto_schema(manual_parameters=[openapi.Parameter('query', openapi.IN_QUERY, description="autocomplete for search", type=openapi.TYPE_STRING), openapi.Parameter('page', openapi.IN_QUERY, description="Page number", type=openapi.TYPE_INTEGER), openapi.Parameter('page_size', openapi.IN_QUERY, description="Items per page (max: 100)", type=openapi.TYPE_INTEGER)], operation_id="Auocomplete Products", operation_description="Search products for autocomplete", tags=["Product"])
     @action(detail=False, methods=['get'], url_path='autocomplete')
     def autocomplete(self, request, *args, **kwargs):
