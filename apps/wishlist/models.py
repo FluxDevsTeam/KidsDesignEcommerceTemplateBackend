fb039940db42b75b9a5c8802fd6b50b0cd21d3eb
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-06-02 23:31:16 +0100

    made wishkist date a datetime field so i can order by time

--- apps/wishlist/models.py
+++ apps/wishlist/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class Wishlist(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="wishlist_product")
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="wishlist_user")
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = ("user", "product")
