8aaa400f41b80e68d7ab2fc84fd2db0c891fdda3
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-12-26 21:44:11 +0100

    feat(api): add new URL paths for blog, consultation, newsletter, and packages
    - Added routes for blog, consultation, newsletter, and packages in api/v1/urls.py
    - Created new blog app with __init__.py
    - Updated requirements.txt with new dependencies including beautifulsoup4, google-auth, and others for enhanced functionality

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', include('apps.ecommerce_admin.urls')),
+    path('blog/', include('apps.blog.urls')),
     path('cart/', include('apps.cart.urls')),
+    path('consultation/', include('apps.consultation.urls')),
+    path('newsletter/', include('apps.newsletter.urls')),
     path('orders/', include(('apps.orders.urls', 'orders'), namespace='orders')),
+    path('packages/', include('apps.packages.urls')),
     path('payment/', include(('apps.payment.urls', 'payment'), namespace='payment')),
     path('product/', include('apps.products.urls')),
     path('wishlist/', include('apps.wishlist.urls')),
