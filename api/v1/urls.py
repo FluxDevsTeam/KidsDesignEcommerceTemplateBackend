2a63349f1f5e3e1be00d9c8b7960051b978b5305
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-12-29 08:48:21 +0100

    feat: add URL path for past_projects API

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('newsletter/', include('apps.newsletter.urls')),
     path('orders/', include(('apps.orders.urls', 'orders'), namespace='orders')),
     path('packages/', include('apps.packages.urls')),
+    path('past_projects/', include('apps.past_projects.urls')),
     path('payment/', include(('apps.payment.urls', 'payment'), namespace='payment')),
     path('product/', include('apps.products.urls')),
     path('wishlist/', include('apps.wishlist.urls')),
